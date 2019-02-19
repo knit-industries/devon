e3600726c0d74db9ccf585732b6ee2714035ee65
bitbonk <bitbonk@msn.com>
2019-02-19 23:09:32 +0100

    Add basic scaffold for HTTP server

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Devon
+
+This is the HTTP server that servers UIs (e.g. for diagnostics) and higher level APIs to control the Brother knitting machine.
---
+++ src/KnitIndustries.KnitRider.Devon.Server/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KnitIndustries.KnitRider.Devon.Server.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
